0df131282c43829642ee1904d2b38055dff20155
Prateek Taneja <75838387+prateektaneja99@users.noreply.github.com>
2023-08-01 18:50:03 -0700

    Search Bar Completed

--- components/index.ts
+++ components/index.ts
@@ -5,4 +5,16 @@ import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer };
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
